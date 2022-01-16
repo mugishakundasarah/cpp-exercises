81c1abe23cc1a89e7cfaec24944e18a85aafb4f2
mugishakundasarah <mugishakundasarah@gmail.com>
2022-01-16 07:58:02 +0200

    changed output form ofthe common elements in 2 arrays program

--- commonElements.cpp
+++ commonElements.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int* commonElem(int* first, int firstSize , int* last, int lastSize){
+void commonElem(int* first, int firstSize , int* last, int lastSize){
     int arr[firstSize];
     int *ptr;
     int count = 0;
@@ -19,18 +19,17 @@ int* commonElem(int* first, int firstSize , int* last, int lastSize){
             
         }   
     }
-    ptr = arr;
-    return ptr;
+    cout << " { ";
+    for (int k = 0; k < count; k++)
+    {
+        cout << arr[k] << " ";
+    } 
+    cout << "} " << endl;
 }
 
 int main(){
     int length = 4;
     int firstArr[4] = {1, 8, 3, 2};
     int secondArr[4] = {4, 2, 6, 1}; 
-    int *res = commonElem(firstArr, length, secondArr, length);
-    for (int k = 0; k < length; k++)
-    {
-        cout << *(res + k);
-    } 
-    
+    commonElem(firstArr, length, secondArr, length);
 }
