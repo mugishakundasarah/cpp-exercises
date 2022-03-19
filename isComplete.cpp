b056b49f7f6053dfdbe5fb796fb1ba00d4c114a6
mugishakundasarah <mugishakundasarah@gmail.com>
2022-03-19 21:58:29 +0200

    started the iscomplete question

---
+++ isComplete.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int isComplete(int *arr, int arrSize){
+    int min;
+    int max;
+    
+    for (int i = 0; i < arrSize; i++)
+    {
+        if (arr[i] % 2 == 0);
+        {
+            if(arr[i] < min){
+                
+            }
+        }
+        
+    }
+    
+    return 0;
+}
+
+int main(){
+
+}
