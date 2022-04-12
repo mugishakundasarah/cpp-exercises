9eb10e441a3165d39889915ff7e47a7f2b6c8db8
mugishakundasarah <mugishakundasarah@gmail.com>
2022-04-12 21:03:24 +0200

    debugged isBean array

--- isBean.cpp
+++ isBean.cpp
@@ -5,23 +5,34 @@ int isBean(int *arr, int length) {
     if(length <= 1){
         return 0;
     }
+    int checkBeanArray = 0;
     for (int i = 0; i < length; i++)
     {
         for (int j = 0; j < length; j++)
         {
             if (arr[i] == arr[j]+1 || arr[i] == arr[j]-1)
             {
-                continue;
-            }
-            else {
-                return 0;
-            }
+                checkBeanArray = 1;
+            }  
         }
-        return 1;
+        if (checkBeanArray == 0 )
+        {
+            return 0;
+        }
+        checkBeanArray = 0;    
     }
-    
+    return 1;
 }
 
 int main(){
-
+    int arrSize = 4;
+    int arr[arrSize] ={2, 10, 8, 3};
+    int result = isBean(arr, arrSize); 
+    if (result)
+    {
+        cout << "It is a bean array" << endl;
+    }else{
+        cout << "It is not a bean array " << endl;
+    }
+       
 }
