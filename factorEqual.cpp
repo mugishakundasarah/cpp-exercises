40c755795fe9896a6e2dd74a03211d366873ff9f
mugishakundasarah <mugishakundasarah@gmail.com>
2022-04-14 00:28:17 +0200

    added factor equal logic

--- factorEqual.cpp
+++ factorEqual.cpp
@@ -2,12 +2,50 @@
 using namespace std;
 
 int factorEqual(int m, int n){
-    return 0;
+    if (m < 2 || n < 2)
+    {
+        return 0;
+    }
+    int countM = 2;
+    int countN = 2;
+
+    int factorNumberM = 0 ;
+    int factorNumberN = 0;
+
+    while (countM <= m / 2)
+    {
+        if (m % countM == 0)
+        {
+            factorNumberM ++;
+        }
+        countM ++; 
+    }
+    
+    while (countN <= n / 2)
+    {
+        if (n % countN == 0)
+        {
+            factorNumberN ++;
+        }
+
+        if (factorNumberN <= factorNumberM)
+        {
+            countN++;
+        }else if(factorNumberN > factorNumberM){
+            return 0;
+        }
+    }
+    if (factorNumberM > factorNumberN)
+    {
+        return 0;
+    }
+    
+    return 1;
 }
 
 int main(){
-    int n1 = 9;
-    int n2 = 33;
+    int n1 = 3;
+    int n2 = 11;
     int result = factorEqual(n1, n2);
      if (result)
     {
