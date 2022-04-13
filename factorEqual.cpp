101fb48496dd62148fd00c6a6d7b11cf0be784b2
mugishakundasarah <mugishakundasarah@gmail.com>
2022-04-13 23:35:53 +0200

    added base code of is factor equal

---
+++ factorEqual.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int factorEqual(int m, int n){
+    return 0;
+}
+
+int main(){
+    int n1 = 9;
+    int n2 = 33;
+    int result = factorEqual(n1, n2);
+     if (result)
+    {
+        cout <<  n1 << " and " << n2 << " are factor equal " << endl;
+    }else{
+        cout <<  n1 << " and " << n2 << " are not factor equal " << endl;
+    }
+}
