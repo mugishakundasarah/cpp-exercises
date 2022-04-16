6db329fea69966fee8de9ac00724248234bba6c9
mugishakundasarah <mugishakundasarah@gmail.com>
2022-04-16 23:47:34 +0200

    finished working on meeraArray

--- meeraArray.cpp
+++ meeraArray.cpp
@@ -16,12 +16,45 @@ using namespace std;
     return true;
 }
 
-int findZero(){
+int findZero(int *arr, int length){
+    for (int i = 0; i < length; i++)
+    {
+        if (arr[i] == 0)
+        {
+            return 1;
+        }
+        
+    }
+    return 0;
+}
 
+int checkMeeraArray(int *arr, int length){
+    
+    int hasZero = findZero(arr, length);
+    if(length < 2){
+        return 0;
+    }
+
+    for (int i = 0; i < length; i++)
+    {
+        if (isPrime(arr[i]) && hasZero)
+        {
+            return 1;
+        }
+        
+    }
+    return 0;
 }
 
 
-
 int main(){
-
+    int length = 3;
+    int meeraArray[length] = {7, 6, 10};
+    int result = checkMeeraArray(meeraArray, length);
+    if (result)
+    {
+        cout << " Is a meera array " << endl;
+    }else{
+        cout << " It is not a meera array " << endl;
+    }
 }
