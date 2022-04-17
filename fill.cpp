8e3354026a8cd4357664844ed6a7ad24793a2f8a
mugishakundasarah <mugishakundasarah@gmail.com>
2022-04-17 23:11:19 +0200

    started working on fill array logic

---
+++ fill.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int* fill(int *arr, int k, int n){
+    if (n < k)
+    {
+        cout << " Elements " << k << " are less than array length " << n << endl;
+        return NULL;
+    }
+    
+    int generalCount = 0;
+
+    for (int i = 0; i < k; i++)
+    {
+        
+    }
+    
+}
+int main(){}
