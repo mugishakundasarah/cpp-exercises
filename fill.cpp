69aecfd84f233b8f9e9aa35f7287c92c1ddce3cc
mugishakundasarah <mugishakundasarah@gmail.com>
2022-04-18 19:00:32 +0200

    still working on fill

--- fill.cpp
+++ fill.cpp
@@ -9,11 +9,28 @@ int* fill(int *arr, int k, int n){
     }
     
     int generalCount = 0;
-
-    for (int i = 0; i < k; i++)
-    {
-        
+    int isFull = 0;
+    int i = 0;
+    while(i<k && generalCount+1 != n){
+        arr[generalCount] = arr[i];
+        i++;
+        if(i == k){
+            i = 0;
+        }
+        generalCount++;
     }
     
 }
-int main(){}
+int main(){
+    int length = 8;
+    int array[length] = {1,2,3,5, 9, 12,-2,-1};
+    int* arr = array;
+    int k = 8;
+    fill(arr, k, length);
+
+    for (int i = 0; i < length; i++)
+    {
+        cout << arr[i] << endl;
+    }
+    
+}
