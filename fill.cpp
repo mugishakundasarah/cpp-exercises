f5ced2e846adbfadc280071a722908ec3ac7a21c
mugishakundasarah <mugishakundasarah@gmail.com>
2022-04-19 20:50:50 +0200

    still working on fill

--- fill.cpp
+++ fill.cpp
@@ -1,36 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int* fill(int *arr, int k, int n){
-    if (n < k)
+int* fill(int *arr, int k, int n){\
+    int j=0;
+    for (int i = 0; i < n; i++)
     {
-        cout << " Elements " << k << " are less than array length " << n << endl;
-        return NULL;
-    }
-    
-    int generalCount = 0;
-    int isFull = 0;
-    int i = 0;
-    while(i<k && generalCount+1 != n){
-        arr[generalCount] = arr[i];
-        i++;
-        if(i == k){
-            i = 0;
+        if(k>0){
+            arr[i] = arr[j];
+            j++;
+            if (j>=k){
+                j = 0;
+            }
         }
-        generalCount++;
     }
-    
+    return arr;
 }
+
 int main(){
     int length = 8;
     int array[length] = {1,2,3,5, 9, 12,-2,-1};
-    int* arr = array;
     int k = 8;
-    fill(arr, k, length);
+    // int* newArr = fill(array, k, length);
 
     for (int i = 0; i < length; i++)
     {
-        cout << arr[i] << endl;
+        cout << array[i] << endl;
     }
     
 }
